770b3c2ddf13e99bcb84bb65d94616e935eda102
Edmilson Rodrigues <edmilson.rodriguesn38@gmail.com>
2024-10-01 11:43:05 +0100

    Using context manager

---
+++ models/html_tags.md
@@ -0,0 +1,3 @@
+<span class="sd-cmp-1jLDJ sd-cmp-fuQAp sd-cmp-3_LLS" tabindex="-1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Accept all</font></font></span>
+
+
--- models/web_scrapper.py
+++ models/web_scrapper.py
@@ -7,6 +7,35 @@ import time
 
 
 class WebScrapper:
-    def __init__(self) -> None:
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    def __init__(self, url: str) -> None:
+        self.options = webdriver.ChromeOptions()
+        # self.options.add_argument("--headless")
+        # self.options.add_argument("--no-sandbox")
+        # self.options.add_argument("--disable-dev-shm-usage")
+        self.url = url
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def __enter__(self):
+        self.open()
+
+    def close(self):
+        self.driver.quit()
+
+    def __exit__(self, **kwargs):
+        self.close()
+
+    def test_scraping(self):
+        time.sleep(5)
+        titles = self.driver.find_elements(By.TAG_NAME, "h2")
+        for title in titles:
+            print(title.text.strip())
+
+
+
+if __name__ == "__main__":
+    with WebScrapper("https://reva.immo/en/") as scrapper:
+        scrapper.test_scraping()
 
